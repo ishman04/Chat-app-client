fc8194548f74c1e2d45ea77eb7dec2309fe80dc6
Ishman Jeet Singh <ishmanalagh@gmail.com>
2025-05-11 12:49:59 +0530

    basic dependencies and structure added

---
+++ src/pages/auth/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Auth = ()=>{
+    return (
+        <div className='h-[100vh] w-[100vw] flex items-center justify'>
+            Auth 
+        </div>
+    )
+}
+
+export default Auth
