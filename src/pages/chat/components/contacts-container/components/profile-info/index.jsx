ed943a3e75be5f502a1ebb7e89f1ccd78fecf958
Ishman Jeet Singh <ishmanalagh@gmail.com>
2025-06-11 09:28:19 +0530

    Added logout button on chat page

--- src/pages/chat/components/contacts-container/components/profile-info/index.jsx
+++ src/pages/chat/components/contacts-container/components/profile-info/index.jsx
@@ -1,10 +1,36 @@
 import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
 import { getColor } from "../../../../../../lib/utils";
 import { useAppStore } from "../../../../../../store";
-import { HOST } from "../../../../../../utils/constants";
+import { HOST, LOGOUT_ROUTE } from "../../../../../../utils/constants";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
+import { FiEdit2 } from "react-icons/fi";
+import { IoLogOut, IoPowerSharp } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
+import { apiClient } from "../../../../../../lib/api-client";
+import { toast } from "sonner";
+import { StatusCodes } from "http-status-codes";
 
 const ProfileInfo = () => {
-  const { userInfo } = useAppStore();
+    const navigate = useNavigate()
+  const { userInfo, setUserInfo } = useAppStore();
+  const handleLogout = async() =>{
+    try {
+        const response = await apiClient.post(LOGOUT_ROUTE,{},{withCredentials:true})
+        if(response.status===StatusCodes.OK){
+            setUserInfo(null)
+            navigate('/auth')
+            toast.success('Logged out successfully')
+        }
+    } catch (error) {
+        console.log(error)
+        toast.error("Something went wrong")
+    }
+  }
   return (
     <div className="absolute bottom-0 left-0 h-16 w-full px-5 flex items-center justify-start bg-neutral-900">
       <div className="flex gap-3 items-center justify-center">
@@ -30,10 +56,44 @@ const ProfileInfo = () => {
           </Avatar>
         </div>
         <div>
-            {
-                userInfo.firstName && userInfo.lastName ? <div className="text-slate-100 poppins-light ">{`${userInfo.firstName} ${userInfo.lastName}`}</div> : ""
-            }
+          {userInfo.firstName && userInfo.lastName ? (
+            <div className="text-slate-100 poppins-regular text-sm">{`${userInfo.firstName} ${userInfo.lastName}`}</div>
+          ) : (
+            ""
+          )}
         </div>
+      </div>
+      <div className="flex gap-5">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <FiEdit2 className="text-gray-500 text-xl font ml-3"
+              onClick={()=>navigate('/profile')}
+              />
+            </TooltipTrigger>
+            <TooltipContent
+              side="top"
+              className="bg-[#1f1f1f] text-slate-100  px-2 py-1.5 mb-1.5 rounded-md shadow-lg text-sm font-medium "
+            >
+              <p>Edit profile</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <IoPowerSharp className="text-red-500 text-xl font ml-3"
+              onClick={handleLogout}
+              />
+            </TooltipTrigger>
+            <TooltipContent
+              side="top"
+              className="bg-[#1f1f1f] text-slate-100  px-2 py-1.5 mb-1.5 rounded-md shadow-lg text-sm font-medium "
+            >
+              <p>Logout</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
     </div>
   );
